b5813da00be01cd5c0337a8ef4dc9888fd536438
leehyeonjunD <leehyeonjun0215@gmail.com>
2017-08-03 17:33:50 +0900

    pf

--- portfolio_page/js/guess_main.js
+++ portfolio_page/js/guess_main.js
@@ -1,11 +1,10 @@
-<!-- script -->
+/* jquery 영역 */
+(function($){
+  var btnMenu = $('#btn_menu');
+  var nav = $('.navigation');
+  // nav클릭 이벤트 ( .on() )
+  btnMenu.on('click', function(){
+    nav.slideToggle();
+  });
 
-  (function($){
-    var btnMenu = $('#btn_menu');
-    var nav = $('.navigation');
-    // gnbList클릭 이벤트 ( .on() )
-    btnMenu.on('click', function(){
-      nav.slideToggle();
-    });
-
-  })(this.jQuery);
+})(this.jQuery);
