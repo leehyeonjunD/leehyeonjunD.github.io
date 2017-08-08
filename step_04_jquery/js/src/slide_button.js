ee1071e64bb1bbb46275c78f10b0a0b0a1d6a1fb
leehyeonjunD <leehyeonjun0215@gmail.com>
2017-08-08 11:46:45 +0900

    jq

---
+++ step_04_jquery/js/src/slide_button.js
@@ -0,0 +1,33 @@
+// slide_button.js
+
+(function($){
+// ---------------------------------------------------
+
+  /*
+    1. .me_link이름을 .slide_btn>ul에 적용
+    2. .slide_btn>ul>li의 가로값을 세로에도 적용(가로 == 세로)
+  */
+
+// 1
+// show/hide -  fadeIn/fadeOut - slideDown/slideUp
+// addClass/removeClass
+
+$('.slide_btn>ul').addClass('me_link');
+
+// 2
+// .slide_btn>ul>li의 가로값
+
+// $('.slide_btn>ul>li').css('width');       // 기본 가로값 파악
+// $('.slide_btn>ul>li').width();            // 기본 가로값
+// $('.slide_btn>ul>li').innerWidth();       // padding 포함
+// $('.slide_btn>ul>li').outerWidth();       // border 포함
+// $('.slide_btn>ul>li').outerWidth(true);   // margin 포함
+
+var slide_li_width = $('.slide_btn>ul>li').width();
+$('.slide_btn>ul>li').height(slide_li_width);
+
+
+
+
+// ---------------------------------------------------
+})(this.jQuery);
