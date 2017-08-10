2470aae272791b39ca1695af0e79110a34817624
leehyeonjunD <leehyeonjun0215@gmail.com>
2017-08-10 12:13:42 +0900

    pfandjqstudy

--- step_04_jquery/js/src/slideBanner_01.js
+++ step_04_jquery/js/src/slideBanner_01.js
@@ -2,13 +2,41 @@
 
 (function($) {
   // slide banner 1
-  // 1. 처음에 보이는 이미지가 첫번째 이미지로 보이기
-  // 2. 오른 버튼을 클릭하며나, 오른쪽 이미지가 보이게 이동
-  // 3. 여러번 클릭해도 계속 보이게 만들기
-  // 4. 빠르게 클릭하면 생기는 메모리 기능이 사라지게 되도록 처리
+  var bannerBox    = $('#addBannerBox');
+  var banner_group = bannerBox.children('ul');
+  var btn          = $('.btn');
+  var btn_l        = btn.children('button:first');
+  var btn_r        = btn.children('button:last');
+  var timed        = 500;
+
+  bannerBox.css({overflow:'hidden'});
+  banner_group.css({marginLeft:'-100%'});
+
+  var li_last      = banner_group.children('li').last();
+  banner_group.prepend(li_last);
+
+  btn_r.on('click',function(e){
+    e.preventDefault();
+    banner_group.stop().animate({marginLeft:'-200%'}, timed, function(){
+      var li_fr    = banner_group.children('li').first();
+        banner_group.append(li_fr);
+        banner_group.css({marginLeft:'-100%'});
+    });
+  });
+
   // 5. 왼쪽 버튼을 클릭하면, 위와 동일한 반복기능 처리
+  btn_l.on('click',function(e){
+    e.preventDefault();
+    banner_group.stop().animate({marginLeft:0}, timed, function(){
+      var li_la = banner_group.children('li').first();
+        banner_group.prepend(li_la);
+        banner_group.css({marginLeft:'-100%'});
+    });
+  });
+
+  // 반복 기능   setInterval()
+
+  // setInterval( btn_r.on('click'), timed );
 
 
-
-  
 })(this.jQuery);
