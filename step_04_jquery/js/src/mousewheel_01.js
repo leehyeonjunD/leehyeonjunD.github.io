2b78beb480da7e3664e8b6b28a94c67fabc53dea
leehyeonjunD <leehyeonjun0215@gmail.com>
2017-08-30 12:21:14 +0900

    jquery13

--- step_04_jquery/js/src/mousewheel_01.js
+++ step_04_jquery/js/src/mousewheel_01.js
@@ -5,9 +5,12 @@
   // para.text('test');
   var html = $('html');
   // var body = $('body');
-  
+
   // html.on('mousewheel DOMMouseScroll', WheelEvent);
-  html.on('mousewheel DOMMouseScroll', WheelEvent);
+  html.on('mousewheel DOMMouseScroll',function(event){
+    var delta = WheelEvent(event);
+    console.log(delta);
+  } );
 
     function WheelEvent(event){
     para.text('마우스 휠 반응');
@@ -16,35 +19,35 @@
     // console.log(event.originalEvent.wheelDeltaY);
 
     // firefox event test
-    console.log(E);
+    // console.log(E);
 
     // firefox DOMMouseScroll 기능에 따라 처리
     if(E.detail){
       delta = E.detail * -40;
-      // console.log(delta);
     }
     // 기타 다른 브라우저
     else{
       delta = E.wheelDeltaY;
-      // console.log(delta);
     }
 
-    // 각 OS별 수치값이 다르기 때문에 동일한 수치로 조정
-    // (삼항식)
+    // 각 OS별 수치값이 다르기 때문에(apple에서는 1의 수치, window에서는 3의 수치) 
+    // 동일한 수치로 조정
     (delta <= 0) ? (delta = -1) : (delta = 1);
 
     // console.log(delta);
-    // (스위치문)
-   switch(delta){
-     case -1:
-       $('body').addClass('down').removeClass('up');
-     break;
+    switch(delta){
+      case -1:
+        $('body').addClass('down').removeClass('up');
+      break;
 
-     case 1:
-       $('body').addClass('up').removeClass('down');
-     break;
-   }
+      case 1:
+        $('body').addClass('up').removeClass('down');
+      break;
+    }
+
+    return delta;
   };
+
  
 
 
